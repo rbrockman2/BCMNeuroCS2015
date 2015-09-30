34926af2c144cb362d4ccba7e26a2da29dd0eca8
Jiakun Fu <jiakun.fu@bcm.edu>
2015-09-30 16:58:03 -0500

    Carli and Jiakun.

--- get_randoms.py
+++ get_randoms.py
@@ -5,7 +5,9 @@ from random import randint
 debug = True
 
 def get_randoms(mean_open_time, mean_close_time, interval):
-    """ Generates open and close times randomly """
+    """ Generates open and close times randomly 
+        Carli and Jiakun are gonna make this the greatst random number generator ever!
+    """
     too_short = True
     samples = 10
     while too_short:
