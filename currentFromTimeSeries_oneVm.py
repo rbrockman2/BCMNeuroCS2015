19df1691e02ce1555f4e71a32d2e79ac1de196b6
Olivia Kim <kim.olivia.a@gmail.com>
2015-09-30 19:23:08 -0500

    Will calculate current given time series, gamma, Vm, and Ex

---
+++ currentFromTimeSeries_oneVm.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 30 19:16:54 2015
+
+@author: Olivia
+"""
+
+'''Take in time series with N channels active in a time bin, single-channel
+conductance, Vm being clamped to, Ex'''
+
+
