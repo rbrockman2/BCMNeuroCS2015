0c58b621c693a618d7f3cb5eafe424fd8a791024
Andrew McKinney <andmmckinney@gmail.com>
2015-09-30 19:41:29 -0500

    betterChannelSummer

--- channelSummer.py
+++ channelSummer.py
@@ -1,10 +1,15 @@
+import Noiseless_Channel
+
 
 def channelSummer(totalTime, nChannels, lifetimeOpenVm, lifetimeClosedVm):
-	# this will be received from somewhere else, eventually. whoever calls it.
-
-	bigTime = []
-	for i in nChannels:
-    	timeSeries = MysteryProgram(lifetimeOpenMs, lifetimeClosedMs, totalTime)
-    	bigSeries = bigSeries + timeSeries
-
-    return bigSeries
+        # this will be received from somewhere else, eventually. whoever calls it.
+        bigSeries = []
+        for i in range(nChannels):
+            timeSeries = Noiseless_Channel(lifetimeOpenVm, lifetimeClosedVm, totalTime)
+            bigSeries = bigSeries + timeSeries
+        return bigSeries
+    
+    
+if __name__ == "__main__": 
+    channelSummer(1000, 1000, 100, 100)
+    
