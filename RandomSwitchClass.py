57c2af35d66d99877b6c96f7fbf17ec4722c9eb5
Robert Theron Brockman II <robert@firehead.org>
2015-10-05 02:24:56 -0500

    RandomSwitch is much more sane now, still needs testing / documentation.

--- RandomSwitchClass.py
+++ RandomSwitchClass.py
@@ -7,94 +7,60 @@ Created on Wed Sep 30 19:18:24 2015
 
 import numpy as np
 from random import randint
-debug = False
 
-
-class RandomSwitch():
+class RandomSwitch():  
+    def __init__(self, open_lifetime_ms=2, closed_lifetime_ms=3):
+        self.open_lifetime = float(open_lifetime_ms) * 1e-3  # convert ms to s
+        self.closed_lifetime = float(closed_lifetime_ms) * 1e-3  # convert ms to s
     
-    def __init__(self, open_lifetime, closed_lifetime):   
-        self.open_lifetime = open_lifetime
-        self.closed_lifetime = closed_lifetime
-
-
-    def input_times(self,time_len = 0.0):
-        """ Take user inputs for open/closed times in msec."""
-        open = self.open_lifetime
-        close = self.closed_lifetime
-        if open:
-            mean_open_time = float(open) * 1e-3 # take ms convert to s
-        else: 
-            mean_open_time = 2e-2 # default
-    								
-        if close:
-            mean_close_time = float(close) * 1e-3 # take ms convert to s
-        else: 
-            mean_close_time = 5e-2 # default
-    
-        if time_len:
-            interval = float(time_len) * 1e-3 # take ms convert to s
-        else: 
-            interval = 500e-3 # default
-            
-        #print(mean_open_time, mean_close_time, interval)   
-        return mean_open_time, mean_close_time, interval
-    
-    @staticmethod
-    def get_randoms(mean_open_time, mean_close_time, interval):
+    def get_randoms(self, run_time):
         """ Generates open and close times randomly """
         too_short = True
         samples = 10
         while too_short:
             # Repeats until we get enough samples
-            opens = np.random.exponential(size=samples)
-            closes = np.random.exponential(size=samples)
-            time_covered = mean_open_time * sum(opens) + mean_close_time * sum(closes)
-            if time_covered > interval:
+            opens = np.random.exponential(size=samples) # random intervals, mean 1
+            closes = np.random.exponential(size=samples) # random intervals, mean 1
+            time_covered = self.open_lifetime * sum(opens) + self.closed_lifetime * sum(closes)
+            if time_covered > run_time:
                 too_short = False
             else:
                 samples *= 2
-        return opens, closes
+        return opens, closes    
     
+    def run_switch_cycle(self, open_time, close_time, dt, g_open=1, start_open=False):
+        switch_data_one_cycle = []
+        n_open_steps = int(open_time/dt)
+        n_close_steps = int(close_time/dt)
+        opening = [g_open for i in range(n_open_steps)]
+        closing = [0 for i in range(n_close_steps)]
+        if start_open:
+            switch_data_one_cycle = opening + closing
+        else:
+            switch_data_one_cycle = closing + opening
+        return switch_data_one_cycle    
     
-    
-    
-    def record_channel(interval, dt, opens, closes, mean_open_time, mean_close_time):        
-        def run_channel(open, closed, g_open=1, start_open=False):
-            channel = []
-            n_open_steps = int(open/dt)
-            n_close_steps = int(closed/dt)
-            opening = [g_open for i in range(n_open_steps)]
-            closing = [0 for i in range(n_close_steps)]
-            if start_open:
-                channel = opening + closing
-            else:
-                channel = closing + opening
-            return channel
-    
-        channel_data = []
+    def record_channel(self,run_time, dt, opens, closes):        
+        switch_data = []
         start_state = randint(0, 1)  # initial state is 0=closed, or 1=open
-        for open, close in zip(opens, closes):
-            open_time = open * mean_open_time
-            close_time = close * mean_close_time
-            channel_data += run_channel(open_time, close_time,
-                                        start_open=start_state)
-            n_steps = len(channel_data) # have to check channel_data instead of summing up open and closed times because in run-channel, int(open/dt) has roundup errors that makes us loose a few data points.
-            if n_steps > int(interval/dt):
+        for single_open, single_close in zip(opens, closes):
+            open_time = single_open * self.open_lifetime  # Scale to average open time.
+            close_time = single_close * self.closed_lifetime  # Scale to average close time.
+            switch_data += self.run_switch_cycle(open_time, close_time, dt, start_open=start_state)
+            n_steps = len(switch_data) # have to check channel_data instead of summing up open and closed times because in run-channel, int(open/dt) has roundup errors that makes us loose a few data points.
+            if n_steps > int(run_time/dt):
                 break
         # chopping it at interval
-        trimmed_data = channel_data[0:int(interval/dt)]
+        trimmed_data = switch_data[0:int(run_time/dt)]
         return trimmed_data
 
-    def run_switch(self, run_time, dt):  # added dt as input to this function instead of having default value
-    
-            
-        # Receive inputs for open, closed, and total times
-        mean_open_time, mean_close_time, interval = self.input_times(run_time)
-        # Concoct random open and closed times    
-        opens, closes = RandomSwitch.get_randoms(mean_open_time, mean_close_time, interval)
+    def run_switch(self, run_time_ms=500, dt_ms=1):  # added dt as input to this function instead of having default value        
+        run_time = run_time_ms * 1e-3  # convert ms to s
+        dt = dt_ms * 1e-3  # convert ms to s
 
-        dt = dt/1000  # convert ms input into seconds units
-        channel_data = RandomSwitch.record_channel(interval, dt, opens,closes,mean_open_time, mean_close_time)
+        # Construct random open and closed times    
+        opens, closes = self.get_randoms(run_time)
+
+        channel_data = self.record_channel(run_time, dt, opens, closes)
 
         return channel_data
-  
